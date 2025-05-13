6bf3e6bdd0bfb3ac95c681debb33c781947c2ca1
enders9245 <juanenderprietoquispe@gmail.com>
2025-05-13 16:32:53 -0500

    Mi segundo commit

---
+++ SysAlmacen/src/test/java/pe/edu/upeu/sysalmacen/repositorio/IMarcaRepositoryTests.java
@@ -0,0 +1,4 @@
+package pe.edu.upeu.sysalmacen.repositorio;
+
+public class IMarcaRepositoryTests {
+}
---
+++ SysAlmacen/src/test/java/pe/edu/upeu/sysalmacen/servicio/MarcaServiceTests.java
@@ -0,0 +1,4 @@
+package pe.edu.upeu.sysalmacen.servicio;
+
+public class MarcaServiceTests {
+}
